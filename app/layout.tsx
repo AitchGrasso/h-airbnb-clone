f46f6f96103f2b364bed515f0eb507b6299d7b2d
H Grasso: PC <aitch.grasso@gmail.com>
2024-02-04 19:53:49 -0600

    finish nav design, modal function

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next'
 import Navbar from './components/navbar/Navbar'
+import Modal from './components/modals/Modal'
+import ClientOnly from './components/ClientOnly'
 import { Nunito } from 'next/font/google'
 import './globals.css'
 
@@ -22,7 +24,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <Navbar />
+          <Modal />
+        </ClientOnly>
         {children}
         </body>
     </html>
